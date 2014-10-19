a00cf28b30fab09b65f7554f654420eab22f54e4
Gwen Pfetsch <gwen.pfetsch@gmail.com>
2014-10-19 00:50:38 -0400

    setting up

---
+++ js/controllers.js
@@ -0,0 +1,32 @@
+'use strict'
+
+var todoListApp = angular.module('todoSharedApp', []);
+
+todoListApp.controller('TodoListCtrl', function($scope) {
+	$scope.user = {name: 'Gwen'};
+
+    $scope.todos = [
+      {text:'learn angular', done:true, claim:false, low:false},
+      {text:'build an angular app', done:false, claim:false, low:false}];
+ 
+    $scope.addTodo = function() {
+      $scope.todos.push({text:$scope.todoText, done:false, postedBy:$scope.user});
+      $scope.todoText = '';
+    };
+ 
+    $scope.remaining = function() {
+      var count = 0;
+      angular.forEach($scope.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    $scope.archive = function() {
+      var oldTodos = $scope.todos;
+      $scope.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) $scope.todos.push(todo);
+      });
+    };
+});
